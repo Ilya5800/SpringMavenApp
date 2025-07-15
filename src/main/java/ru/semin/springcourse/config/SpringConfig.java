13a01129ecda9062c1b5464a8cf6c81cc06ee8c9
Ilya5800 <ilyasemin870@gmail.com>
2025-07-15 12:05:38 +0300

    Урок 8 CRUD,REST, Паттерн DAO на данном уроке было пройдено чтение из дао и передставление только чтение READ

--- src/main/java/ru/semin/springcourse/config/SpringConfig.java
+++ src/main/java/ru/semin/springcourse/config/SpringConfig.java
@@ -3,8 +3,8 @@ package ru.semin.springcourse.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -13,7 +13,7 @@ import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 
 @Configuration
-@Component("ru.semin.sringcourse")
+@ComponentScan("ru.semin.springcourse")
 @EnableWebMvc
 public class SpringConfig implements WebMvcConfigurer {
     private final ApplicationContext applicationContext;
--- src/main/java/ru/semin/springcourse/controllers/PeopleController.java
+++ src/main/java/ru/semin/springcourse/controllers/PeopleController.java
@@ -4,32 +4,59 @@ package ru.semin.springcourse.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.semin.springcourse.dao.PersonDAO;
+import ru.semin.springcourse.models.Person;
 
 @Controller
 @RequestMapping("/people")
 public class PeopleController {
 
     private final PersonDAO personDAO;
-@Autowired
+
+    @Autowired
     public PeopleController(PersonDAO personDAO) {
         this.personDAO = personDAO;
     }
 
     @GetMapping()
-    public String index(Model model){
-        //Получим всех людей их DAO и передадим на отображение в представлении
-        model.addAttribute("people",personDAO.index());
+    public String index(Model model) {
+        model.addAttribute("people", personDAO.index());
         return "people/index";
     }
 
     @GetMapping("/{id}")
-    public String show(@PathVariable("id") int id, Model model){
-        //Получим одного человека по его id  bp dao и передадим этого человека для отображения
-        model.addAttribute("person",personDAO.show(id));
-      return "people/show";
+    public String show(@PathVariable("id") int id, Model model) {
+        model.addAttribute("person", personDAO.show(id));
+        return "people/show";
+    }
+
+    @GetMapping("/new")
+    public String newPerson(@ModelAttribute("person") Person person) {
+        return "people/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("person") Person person) {
+        personDAO.save(person);
+        return "redirect:/people";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String edit(Model model, @PathVariable("id") int id) {
+        model.addAttribute("person", personDAO.show(id));
+        return "people/edit";
+    }
+
+    @PatchMapping("/{id}")
+    public String update(@ModelAttribute("person") Person person, @PathVariable("id") int id) {
+        personDAO.update(id, person);
+        return "redirect:/people";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") int id) {
+        personDAO.delete(id);
+        return "redirect:/people";
     }
 }
